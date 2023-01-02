39ab64f53816ebe438f370ff3426c434931f584a
divakar bhardwaj <dbhardwaj@contecglobal.com>
2023-01-02 22:07:00 +0530

    added icon styles

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,85 +1,102 @@
-import styled, { ThemeProvider } from 'styled-components'
-import { color, space, fontSize, buttonStyle, variant, width } from 'styled-system'
+import Icon from '@mdi/react'
 import PropTypes from 'prop-types';
-
-const buttonSize = variant({
-  prop: 'size',
-  key: 'buttonSizes'
-})
-
-// export const theme = {
-//   colors: {
-//     custom: '#444',
-//     yellow: 'yellow'
-//   },
-//  buttons: {
-//     primary: {
-//       color: 'white',
-//       backgroundColor: '#2962FF'
-//     },
-//     secondary: {
-//       color: 'white',
-//       backgroundColor: '#455A64'
-//     },
-//     danger: {
-//       color: 'white',
-//       backgroundColor: '#D32F2F'
-//     }  
-//   },
-// buttonSizes: {
-//     small: {
-//       fontSize: '14px',
-//       padding: `6px 0.95em`,
-//       width: '73px',
-//       heigh: '32px'
-//     },
-//     medium: {
-//       fontSize: '14px',
-//       padding: `8px 16px`,
-//       width: '81px',
-//       height: '36px'
-//     },
-//     large: {
-//       fontSize: '14px',
-//       padding: `11px 22px`,
-//       width: '93px',
-//       height: '42px'
-//     }
-//   }
-// }
+import "./Button.css";
 
 const variantArray = {
   "text" : {
     cursor: 'default',
-    backgroundColor: 'white'
+    backgroundColor: 'white',
+    boxShadow: 'none'
   },
   "outline" : {
     borderWidth: '1px',
   }
 }
+
+const colorObject = {
+  "primary": {
+    color: 'white',
+    backgroundColor: '#2962FF'
+  },
+  "secondary": {
+    color: 'white',
+    backgroundColor: '#455A64'
+  },
+  "danger": {
+    color: 'white',
+    backgroundColor: '#D32F2F'
+  }
+}
+
+const sizeObject = {
+  "sm": {
+    width: '73px',
+    height: '32px'
+  },
+  "md": {
+    width: '81px',
+    height: '36px'
+  },
+  "lg": {
+    width: '93px',
+    height: '42px'
+  }
+}
+
 const defaultButtonStyle = {
   fontSize: '14px',
   padding: `8px 16px`,
   width: '81px',
   height: '36px',
+  fontWeight: '500',
   backgroundColor: '#E0E0E0',
   borderRadius: '6px',
   borderWidth: '0px',
   borderColor: '',
-  cursor: 'pointer'
+  cursor: 'pointer',
+  boxShadow: '0px 2px 3px 0px #33333333',
+  position: 'relative'
 }
 
-export const Button = ({variant, color, size, text="Default"}) => {
-
+export const Btn = (props) => {
+  console.log(props);
+  return(
+    <button {...props}>
+      text
+    </button>
+  )
+}
+export const Button = ({variant, color, size, text="Default", leftIcon, rightIcon, ...props}) => {
+  console.log(props)
   const computedStyle = () => {
+    const width = (leftIcon || rightIcon) && {width: '105px'};
     return{
       ...defaultButtonStyle,
       ...variantArray[variant],
+      ...colorObject[color],
+      ...sizeObject[size],
+      ...width
     }
   }
+
   return(
-    <button style={computedStyle()}>
+    <button {...props} style={computedStyle()}>
+     
+      {leftIcon && 
+        <Icon path={leftIcon} 
+        className='left-icon'
+        rotate={180}
+        size={0.75}
+        horizontal
+        vertical />}
       {text}
+      {rightIcon && 
+        <Icon path={rightIcon} 
+        className='right-icon'
+        rotate={180}
+        size={0.75}
+        horizontal
+        vertical />}
     </button>
   )
 }
@@ -90,8 +107,8 @@ Button.propTypes = {
   size: PropTypes.oneOf(['sm', 'md', 'lg'])
 }
 
-Button.defaultProps = {
-  variant: 'primary',
-  backgroundColor: 'blue',
-  size: 'medium'
-}
+// Button.defaultProps = {
+//   variant: 'primary',
+//   backgroundColor: 'blue',
+//   size: 'medium'
+// }
